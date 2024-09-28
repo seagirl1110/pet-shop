4629ba3a2f67591f4efc8253b45414f44d14edf0
Marina Elizarova <seagirl1110@gmail.com>
2024-09-28 13:06:20 +0300

    feat: add header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,41 @@
+import styles from './styles.module.css';
+import { Link, NavLink } from 'react-router-dom';
+import logo from './../../assets/icons/logo.svg';
+import cart from './../../assets/icons/cart.svg';
+
+const navList = [
+  { name: 'Main Page', path: '/' },
+  { name: 'Categories', path: '/categories' },
+  { name: 'All products', path: '/products' },
+  { name: 'All sales', path: '/sale' },
+];
+
+function Header() {
+  return (
+    <header className={styles.header_container}>
+      <Link to="/">
+        <img src={logo} alt="Pet_shop_logo" />
+      </Link>
+      <nav className={styles.nav_container}>
+        {navList.map(({ name, path }) => (
+          <NavLink
+            key={name}
+            to={path}
+            className={({ isActive }) =>
+              isActive
+                ? `${styles.nav_link} ${styles.nav_link__active}`
+                : `${styles.nav_link}`
+            }
+          >
+            {name}
+          </NavLink>
+        ))}
+      </nav>
+      <div>
+        <img src={cart} alt="cart" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
